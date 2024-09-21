2122d7d4d4510d68161fd1ed98c8925534aea304
hi <you@example.com>
2024-09-21 16:56:36 -0600

    feature: updated end screen

--- scripts/crosshair.gd
+++ scripts/crosshair.gd
@@ -63,21 +63,7 @@ func _process(delta: float) -> void:
 				direction.y = 1
 			else:
 				direction.y = -1
-		
-	#if Input.is_action_pressed("ui_right"):
-		#direction.x = 1
-	#elif Input.is_action_pressed("ui_left"):
-		#direction.x = -1
-	#else:
-		#direction.x = 0
-	#
-	#if Input.is_action_pressed("ui_down"):
-		#direction.y = 1
-	#elif Input.is_action_pressed("ui_up"):
-		#direction.y = -1
-	#else:
-		#direction.y = 0
-
+				
 	position += direction * speed * delta
 
 			
