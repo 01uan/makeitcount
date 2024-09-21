e1f0ae2e39ee2e33852705a35f3a9a1d7d787982
hi <you@example.com>
2024-09-21 14:15:55 -0600

    feature: added explosion animation

--- scripts/HitZone.gd
+++ scripts/HitZone.gd
@@ -1,10 +1,18 @@
 extends Sprite2D  # Assuming this script is attached to the hitzone sprite
 
 var points: int = 0
+var explosion_sprite: AnimatedSprite2D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+
+func _ready():
+	explosion_sprite = get_node("../Explosion")  # Replace with the correct path to your explosion sprite
+	explosion_sprite.hide()
+	explosion_sprite.connect("animation_finished", Callable(self, "_on_explosion_animation_finished"))
+	
 func _process(delta):
 	if is_crosshair_in_hitzone() and Input.is_action_just_pressed("ui_select"):
 		hide()
+		play_explosion_animation()
 		increment_point()
 		random_reposition()
 		show() 
@@ -19,13 +27,21 @@ func is_crosshair_in_hitzone() -> bool:
 	return distance < 15
 	
 # Reposition the hitzone randomly within the screen bounds
-func random_reposition():
-	global_position = Vector2(randi() % 1000, randi() % 500) # Random position based on screen size
+func random_reposition(): 
+	global_position = Vector2(randi() % 1000, 150 + randi() % 450) # Random position based on screen size
 
 func increment_point():
 	points += 1
 	update_points_label()
-	
+
+func play_explosion_animation():
+	explosion_sprite.global_position = global_position
+	explosion_sprite.show()
+	explosion_sprite.play("default")
+
+func _on_explosion_animation_finished():
+	explosion_sprite.hide()  # Hide the explosion sprite after the animation is done
+
 func update_points_label():
 	$"../Hud".points = points
 	#var points_label = get_node("../PointsLabel")
