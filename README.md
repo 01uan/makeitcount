6c7cd2eac540974576afd66b0fcad2f23ebee560
hi <you@example.com>
2024-09-20 20:49:06 -0600

    feature: added crosshair movement

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 - [ ] integrate morgans drawing as monster
 - [ ] add a timer
-- [ ] add a crosshair, map it to arrow keys
 - [ ] randomize hitmarker on hitbox of monster
-- [ ] space to shoot and make hitmarker disapear and reappear somewhere else 
+- [ ] space to shoot and make hitmarker disapear and reappear somewhere else
+- [ ] add some animation to firing hitmarker 
---
+++ crosshair.gd
@@ -0,0 +1,23 @@
+extends Sprite2D
+
+var speed: float = 200
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var direction = Vector2.ZERO
+	
+	if Input.is_action_pressed("ui_right"):
+		direction.x = 1
+	elif Input.is_action_pressed("ui_left"):
+		direction.x = -1
+	else:
+		direction.x = 0
+	
+	if Input.is_action_pressed("ui_down"):
+		direction.y = 1
+	elif Input.is_action_pressed("ui_up"):
+		direction.y = -1
+	else:
+		direction.y = 0
+
+	position += direction * speed * delta
